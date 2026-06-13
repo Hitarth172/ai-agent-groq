65388736b4079f96c9c3d9c764d9a0a806ef355d
Hitarth Rokadia <hitarthrokadia@gmail.com>
2026-06-13 11:28:45 +0530

    Initial Commit

---
+++ app/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from groq import Groq
+import os
+
+app = FastAPI()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+class ChatMessage(BaseModel):
+    message: str
+
+@app.get("/")
+def home():
+    return {"message": "AI Agent Running"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+@app.post("/chat")
+def chat(data: ChatMessage):
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": data.message
+            }
+        ]
+    )
+
+    return {
+        "response": response.choices[0].message.content
+    }
